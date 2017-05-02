478daf3a9ed2f35013a70bfb38155cd3aaca8707
Istvan Marki <istv0050@edu.easj.dk>
2017-05-02 22:10:49 +0200

    sucessfully added ssl and secure port

--- MockyShopClient/Startup.cs
+++ MockyShopClient/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -16,6 +17,13 @@ namespace MockyShopClient
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(options =>
+            {
+                options.SslPort = 44311;
+                options.Filters.Add(new RequireHttpsAttribute());
+            });
+
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
