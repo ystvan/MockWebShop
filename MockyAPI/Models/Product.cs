f6b31fec73420987426e7ee4a713881b304f66bf
Istvan Marki <istv0050@edu.easj.dk>
2017-05-02 21:05:20 +0200

    hotfix on DB

--- MockyAPI/Models/Product.cs
+++ MockyAPI/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace MockyAPI.Models
 {
     public class Product
     {
+        [Key]
         public Guid Id { get; set; }
         public String Name { get; set; }
         public String Description { get; set; }
